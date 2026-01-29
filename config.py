88bc3226a9d1e61a1f98f78fca149a7b8bf855d3
Vijayavarsini <kjvarsini14@gmail.com>
2026-01-30 01:08:10 +0530

    Use DATABASE_URL from environment variables

--- config.py
+++ config.py
@@ -3,12 +3,12 @@
 import os
 
 DB_USERNAME = "clinicadmin"
-DB_PASSWORD = "vijay@123"
+DB_PASSWORD = "1234Viju*"
 DB_HOST = "pg-clinic-db-varsini.postgres.database.azure.com"
 DB_NAME = "clinicdb"
 
 SQLALCHEMY_DATABASE_URI = (
-    "postgresql+psycopg2://clinicadmin:vijay%40123"
+    "postgresql+psycopg2://clinicadmin:1234Viju%2A"
     "@pg-clinic-db-varsini.postgres.database.azure.com:5432/clinicdb"
     "?sslmode=require"
 )
