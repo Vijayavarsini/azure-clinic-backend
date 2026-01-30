4613549b1f16f790f86481c84c15deedd6ea6fed
Vijayavarsini <kjvarsini14@gmail.com>
2026-01-30 23:49:56 +0530

    Fix Azure crash: remove db.create_all from startup

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 
+BASE_DIR = "/home/site/wwwroot"
+DB_PATH = os.path.join(BASE_DIR, "clinic.db")
+
 SQLALCHEMY_DATABASE_URI = os.environ.get(
     "DATABASE_URL",
-    "sqlite:////home/site/clinic.db"
+    f"sqlite:///{DB_PATH}"
 )
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
